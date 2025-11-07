d7ebe0d1e6e8f0ee003dbe1bd1df7da8dbc415bc
loveleo520663 <loveleo520663@gmail.com>
2025-11-08 06:47:05 +0800

    🔍 添加完整管理員 API 診斷工具

--- server/app.js
+++ server/app.js
@@ -239,6 +239,14 @@ app.get('/admin-test', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/admin-test.html'));
 });
 
+app.get('/admin-diagnosis', (req, res) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('ETag', 'false');
+  res.sendFile(path.join(__dirname, '../views/admin-diagnosis.html'));
+});
+
 app.get('/prediction', (req, res) => {
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
